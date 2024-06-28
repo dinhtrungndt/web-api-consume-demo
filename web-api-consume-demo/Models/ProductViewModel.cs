d563d335d0f02a2003c2340008db94451d10b7e7
kho quản lý <nguyendinhtrung.it@gmail.com>
2024-06-28 23:04:18 +0700

    up

---
+++ web-api-consume-demo/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_api_consume_demo.Models
+{
+    public class ProductViewModel
+    {
+    }
+}
